91140c4bfd5b28273337c4f337526e9d619b8393
Prince <12prince345singh@gmail.com>
2025-07-19 19:09:08 +0530

    server is updated

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/meetings', meetingRoutes);
 
+app.set('io', io);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
