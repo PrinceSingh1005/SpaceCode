b5fbb57d84d446d8c9de43ff444a9df55b23b450
Prince <12prince345singh@gmail.com>
2025-08-01 21:15:01 +0530

    mongoDB string problem solved

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ app.use('/api/meetings', meetingRoutes);
 
 app.set('io', io);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI);
 
 setupSocket(io); // Pass io instance to setupSocket
 
